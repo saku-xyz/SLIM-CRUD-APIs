05eaa2c4dfc160dde50757954797e0d4340c95be
SaKu <sakuofficial@gmail.com>
2021-06-01 21:59:47 +0530

    Update customer

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -94,3 +94,49 @@ $app->post('/api/customer/add', function(Request $request, Response $response){
     }
 });
 
+// Update Customer
+$app->put('/api/customer/update/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+    $first_name = $request->getParam('first_name');
+    $last_name = $request->getParam('last_name');
+    $phone = $request->getParam('phone');
+    $email = $request->getParam('email');
+    $address = $request->getParam('address');
+    $city = $request->getParam('city');
+    $state = $request->getParam('state');
+
+    $sql = "UPDATE customers SET
+				first_name 	= :first_name,
+				last_name 	= :last_name,
+                phone		= :phone,
+                email		= :email,
+                address 	= :address,
+                city 		= :city,
+                state		= :state
+			WHERE id = $id";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':first_name', $first_name);
+        $stmt->bindParam(':last_name',  $last_name);
+        $stmt->bindParam(':phone',      $phone);
+        $stmt->bindParam(':email',      $email);
+        $stmt->bindParam(':address',    $address);
+        $stmt->bindParam(':city',       $city);
+        $stmt->bindParam(':state',      $state);
+
+        $stmt->execute();
+
+        echo '{"notice": {"text": "Customer Updated"}';
+
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
