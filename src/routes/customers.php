d32699d30befe77b787c914664c2d2a6156c1121
SaKu <sakuofficial@gmail.com>
2021-06-01 21:59:12 +0530

    Add new customer

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -35,3 +35,62 @@ $app->get('/api/customers', function(Request $request, Response $response){
     }
 });
 
+// Get Single Customer
+$app->get('/api/customer/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+
+    $sql = "SELECT * FROM customers WHERE id = $id";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customer = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($customer);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+// Add Customer
+$app->post('/api/customer/add', function(Request $request, Response $response){
+    $first_name = $request->getParam('first_name');
+    $last_name = $request->getParam('last_name');
+    $phone = $request->getParam('phone');
+    $email = $request->getParam('email');
+    $address = $request->getParam('address');
+    $city = $request->getParam('city');
+    $state = $request->getParam('state');
+
+    $sql = "INSERT INTO customers (first_name,last_name,phone,email,address,city,state) VALUES
+    (:first_name,:last_name,:phone,:email,:address,:city,:state)";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':first_name', $first_name);
+        $stmt->bindParam(':last_name',  $last_name);
+        $stmt->bindParam(':phone',      $phone);
+        $stmt->bindParam(':email',      $email);
+        $stmt->bindParam(':address',    $address);
+        $stmt->bindParam(':city',       $city);
+        $stmt->bindParam(':state',      $state);
+
+        $stmt->execute();
+
+        echo '{"notice": {"text": "Customer Added"}';
+
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
